1feaa8f2424a9a2952ae58d8235f60fce42927bc
Arondriessen <arondriessen@yahoo.com>
2022-07-16 15:32:56 +0100

    Added menus and level system using Webflow.

---
+++ levels.js
@@ -0,0 +1,45 @@
+
+
+levels = [ // Levels
+
+  [ // Level 1
+    [ // Targets
+      // grid x, grid y, number to land on, state (not-solved, solved)
+      [3, 1, 3, 0]
+    ],
+    10, // Number of moves
+    4, // Grid Resolution
+  ],
+
+  [ // Level 2
+    [ // Targets
+      // grid x, grid y, number to land on, state (not-solved, solved)
+      [3, 1, 3, 0],
+      [5, 3, 2, 0]
+    ],
+    20, // Number of moves
+    6, // Grid Resolution
+  ],
+
+  [ // Level 3
+    [ // Targets
+      // grid x, grid y, number to land on, state (not-solved, solved)
+      [3, 1, 3, 0],
+      [6, 3, 2, 0],
+      [4, 3, 4, 0]
+    ],
+    30, // Number of moves
+    8, // Grid Resolution
+  ],
+
+  [ // Level 4
+    [ // Targets
+      // grid x, grid y, number to land on, state (not-solved, solved)
+      [3, 1, 3, 0],
+      [6, 3, 2, 0],
+      [4, 3, 4, 0]
+    ],
+    30, // Number of moves
+    10, // Grid Resolution
+  ]
+]
